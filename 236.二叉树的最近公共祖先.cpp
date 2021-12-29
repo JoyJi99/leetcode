6e1365998708e272489172971f0969b135adf22a
JoyJi99 <65765997+JoyJi99@users.noreply.github.com>
2021-12-29 20:37:25 +0800

    Update 236.二叉树的最近公共祖先.cpp

--- 236.二叉树的最近公共祖先.cpp
+++ 236.二叉树的最近公共祖先.cpp
@@ -17,7 +17,14 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
+        if(!root || root == p || root == q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if(left){
+            if(right) return root;//左右都找到，说明左右子树各一个
+            else return left;//只在右边找到
+        }
+        else return right;//不可能left和right都没有，因此一定是right
     }
 };
 // @lc code=end
