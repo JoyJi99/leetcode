8773901928212df4e5fb8786d7db9efa88d97132
JoyJi99 <jixiang1999@126.com>
2022-01-15 14:02:20 +0800

    977

--- 977.有序数组的平方.cpp
+++ 977.有序数组的平方.cpp
@@ -8,8 +8,32 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        
+        int k = nums.size() - 1;
+        vector<int> res(nums.size(), -1);
+        for(int i = 0, j = nums.size() - 1; i <= j;){
+            if(nums[i]*nums[i] > nums[j]*nums[j]){
+                res[k--] = nums[i]*nums[i];
+                i++; 
+            }
+            else{
+                res[k--] = nums[j]*nums[j];
+                j--;
+            }
+        }
+        return res; 
     }
 };
+/*
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++){
+            nums[i] *= nums[i];
+        }
+        sort(nums.begin(), nums.end());//快速排序 
+        return nums; 
+    }
+};
+*/
 // @lc code=end
 
