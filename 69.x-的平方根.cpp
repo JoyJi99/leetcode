dc4ac8060dada48e69c926fc8aa1f468b64c246a
JoyJi99 <jixiang1999@126.com>
2022-03-25 13:33:35 +0800

    69

--- 69.x-的平方根.cpp
+++ 69.x-的平方根.cpp
@@ -8,7 +8,17 @@
 class Solution {
 public:
     int mySqrt(int x) {
-
+        int left = 0;
+        int right = x;
+        long mid;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (mid * mid > x) right = mid - 1;
+            else if (mid * mid < x) left = mid + 1;
+            else return mid;
+        }
+        if (mid * mid < x && (mid + 1) * (mid + 1) > x) return mid; 
+        else return mid - 1; 
     }
 };
 // @lc code=end
